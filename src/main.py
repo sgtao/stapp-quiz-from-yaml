eec114829a6172db4a5d869bb0d5571dc0cb18e9
Shogo Ogami <sg.tao.so@gmail.com>
2024-12-17 12:57:24 +0900

    Del:delete exapmle_app

--- src/main.py
+++ src/main.py
@@ -8,4 +8,4 @@ Edit `/src` and `/tests` to customize this app to your heart's desire :heart:.
 
 # サイドバーのページに移動
 # st.page_link("pages/example_app.py", label="Go to Example App")
-st.page_link("pages/01_example_app.py", label="Go to Example App", icon="🚀")
+st.page_link("pages/11_quiz_practice_test.py", label="Go to Quiz App", icon="🚀")
